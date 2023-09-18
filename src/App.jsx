9833a0dd502cb4d183a84c62000908d116826ea4
Alan Anguiano <alan.anguiano@valtre.com>
2023-09-18 00:43:38 -0600

    Modifies navbar and index styles and adds Bookdashboard to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import BookDashboard from './components/BookDashboard'
 
 function App() {
   return (
     <>
       <Navbar title="Library App"/>
+      <BookDashboard/>
     </>
   )
 }
